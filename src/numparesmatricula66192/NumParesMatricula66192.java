acf7f0ef5c04dd014d4465de7d3ca0fc969cd839
acs <acs@win10>
2021-02-24 20:56:43 -0600

    primerCommit

---
+++ src/numparesmatricula66192/NumParesMatricula66192.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numparesmatricula66192;
+
+/**
+ *
+ * @author acs
+ */
+public class NumParesMatricula66192 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int n = 0;
+        
+        while (n<94)
+        {
+            if (n%2 == 0){
+                System.out.println("n = " + n);
+            }
+            n++;
+        } 
+        
+        
+    }
+    
+}
